0162ac356e48440bea37c299d8b511afd8f9b7e4
maqweb <balikhinv@bk.ru>
2020-03-07 20:51:15 +0300

    Updates

---
+++ precache-manifest.be323d913a509793950b29cc626d6bd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20ff7c9e6ec50b95f95c9453a181bbc",
+    "url": "/todo-react-app/index.html"
+  },
+  {
+    "revision": "88dd391afa01af267fbc",
+    "url": "/todo-react-app/static/css/main.82dd9dc5.chunk.css"
+  },
+  {
+    "revision": "065a0bff742a4389eed0",
+    "url": "/todo-react-app/static/js/2.5d3108eb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo-react-app/static/js/2.5d3108eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88dd391afa01af267fbc",
+    "url": "/todo-react-app/static/js/main.0a9acd41.chunk.js"
+  },
+  {
+    "revision": "fd6efcd7ef3f47029661",
+    "url": "/todo-react-app/static/js/runtime-main.e54e4c8b.js"
+  }
+]);
